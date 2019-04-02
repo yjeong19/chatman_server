e0efd3d28ff8f10ceca96865a38206c441b7075c
Young Jeong <yjeong19@gmail.com>
2019-04-02 16:51:40 -0700

    red hat

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   // mongoURI: 'mongodb://localhost:27017/chatman',
   secretOrKey: 'secret',
-  mongoURI: 'mongodb://yjeong19:soccer1@ds145574.mlab.com:45574/chatweb'
+  mongoURI: 'mongodb://yjeong19:soccer1@mongodb/chatweb'
 }
